15b0630d86179068cba3736def4d6ceb75d7ce96
Desert Python <mattsenneff@aol.com>
2020-09-12 20:18:53 -0700

    hard knocks

---
+++ models/Image.js
@@ -0,0 +1,30 @@
+// make model for imges
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Image extends Model {}
+Image.init(
+    {
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+    },
+      image_file: {
+        type: DataTypes.BLOB,
+        allowNull:false,
+
+      },
+      plant_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
+      
+  },{
+    sequelize,
+    modelName: 'Image'
+  })
+  
+module.exports = Image;
+
+    

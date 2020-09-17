8c2915e9d45c3748da861dcc8803d3a5c359d52c
Desert Python <mattsenneff@aol.com>
2020-09-16 18:21:19 -0700

    editplanthistoryjsupdate

--- public/javascript/edit-plant-history.js
+++ public/javascript/edit-plant-history.js
@@ -1,37 +1,33 @@
 const newFormHandler = async function(event) {
     event.preventDefault();
-
+    const history_id = document.querySelector('input[name="plant_history_id"]').value;
     const plant_id = document.querySelector('input[name="plant_id"]').value;
-    const recType = document.querySelector('input[name="plant_identifier"]').value;
+    const rec_type = document.querySelector('input[name="rec_type"]').value;
     const loc = document.querySelector('input[name="loc"]').value;
-    const inGround = document.querySelector('select[name="in_ground"]').value;
-    const directSunHours = document.querySelector('input[name="direct_sunlight_hours"]').value;
-    const waterSchedule = document.querySelector('input[name="watering_schedule"]').value;
+    const in_ground = document.querySelector('select[name="in_ground"]').value;
+    const direct_sunlight_hours = document.querySelector('input[name="direct_sunlight_hours"]').value;
+    const watering_schedule = document.querySelector('input[name="watering_schedule"]').value;
     const additional_info = document.querySelector('textarea[name="additional_info"]').value;
-
     const token = localStorage.getItem("token");
-
     await fetch('/edit-plant-history', {
         method: "PUT",
         body: JSON.stringify({
+            history_id,
             plant_id,
-            recType,
+            rec_type,
             loc,
-            inGround,
-            directSunHours,
-            waterSchedule,
+            in_ground,
+            direct_sunlight_hours,
+            watering_schedule,
             additional_info
         }),
         headers: {
             "Content-Type": "application/json",
             authorization: `Bearer ${token}`
         }
-    }).then(res => 
-        res.json()
-    ).then(
-        //once the plant is created redirect user to edit page for the new plant_id
-        data => document.location.replace('http://localhost:3001/edit-plant/' + data.plant_id)
-    );
+    });
+    //once the history record is updated redirect user to edit plant page so they can see their history record
+    alert("The plant has been successfully updated!");
+    data => document.location.replace('http://localhost:3001/edit-plant/' + data.plant_id)
 };
-
 document.querySelector("#edit-plant-history").addEventListener("submit", newFormHandler);
